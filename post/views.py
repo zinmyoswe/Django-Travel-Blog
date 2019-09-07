03f4d8fc2565cc1c024aedaaba433e35c9aae871
ZIN MYO <overmidnight12@gmail.com>
2019-09-07 13:27:19 -0700

    my changes

--- post/views.py
+++ post/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html',{})
+    queryset = Post.objects.all()
+    context = {
+        'object_list' : queryset
+    }
+    return render(request, 'index.html',context)
 
 def blog(request):
     return render(request, 'blog.html',{})
