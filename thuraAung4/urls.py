205fc824742e6bf073aba44febaedcdb529b742a
ZIN MYO <overmidnight12@gmail.com>
2019-09-07 10:57:19 -0700

    my changes

--- thuraAung4/urls.py
+++ thuraAung4/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import index, blog, post
+from post.views import index, blog, post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
