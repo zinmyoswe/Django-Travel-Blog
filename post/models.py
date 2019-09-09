c53fa117226092e1a23cda2437180dc4c073ba40
ZIN MYO <overmidnight12@gmail.com>
2019-09-09 07:37:56 -0700

    my changes

--- post/models.py
+++ post/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 # Create your models here.
 
@@ -23,6 +24,7 @@ class Post(models.Model):
     overview = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     comment_count = models.IntegerField(default = 0)
+    view_count = models.IntegerField(default = 0)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     thumbnail = models.ImageField()
     categories = models.ManyToManyField(Category)
@@ -30,4 +32,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={
+            'id': self.id
+        })
 
--- post/views.py
+++ post/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Post
 from marketing.models import Signup
@@ -21,7 +22,23 @@ def index(request):
     return render(request, 'index.html',context)
 
 def blog(request):
-    return render(request, 'blog.html',{})
+    most_recent = Post.objects.order_by('-timestamp')[:3]
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 4)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
+    context = {
+        'queryset' : paginated_queryset,
+        'most_recent': most_recent,
+        'page_request_var': page_request_var
+    }
+    return render(request, 'blog.html',context)
 
-def post(request):
+def post(request, id):
     return render(request, 'post.html',{})
